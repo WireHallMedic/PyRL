303a42f0861d92d24e70b82a311757c9283f630b
Michael <MichaelWidler@yahoo.com>
2026-08-18 08:05:21 -0700

    Add __init__ for packaging.

---
+++ Engine/utility.py
@@ -0,0 +1,10 @@
+"""
+Collection of helper functions
+"""
+
+def create_2d_array(width, height, default_val=0):
+   """
+   Helper method to create a 2d array of size [width][height]
+   returns -> Object[][]
+   """
+   return [[default_val for _ in range(height)] for _ in range(width)]
