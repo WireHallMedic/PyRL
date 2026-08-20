78f2cacb1299a896d74d0227e542eb2cabda81c8
Michael <MichaelWidler@yahoo.com>
2026-08-20 16:04:42 -0700

    Add scaffolding for camera tracking.

--- GUI/unbound_screen_obj.py
+++ GUI/unbound_screen_obj.py
@@ -14,10 +14,16 @@ class UnboundScreenObj(screen_obj.ScreenObj):
       self.loc_tiles = [-1, -1]
       self.offset = [0.0, 0.0]
    
-   def blit_to_surface(self, surface):
+   def blit_to_surface(self, surface, reference=None):
       """
       Calculates position and blits to passed surface   
+      surface (Surface): surface to blit this image on to
+      reference (UnboundScreenObj) another USO placed at center of surface
       """
-      x_loc = int((self.loc_tiles[0] + self.offset[0]) * self.palette.tile_size_px[0])
-      y_loc = int((self.loc_tiles[1] + self.offset[1]) * self.palette.tile_size_px[1])
+      x_pos_tiles = self.loc_tiles[0]
+      y_pos_tiles = self.loc_tiles[1]
+      if reference is not None:
+         pass # TODO
+      x_loc = int((x_pos_tiles + self.offset[0]) * self.palette.tile_size_px[0])
+      y_loc = int((y_pos_tiles + self.offset[1]) * self.palette.tile_size_px[1])
       surface.blit(self.get_image(), (x_loc, y_loc))
