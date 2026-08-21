2ac3c4f64162d9878ef2adbcecd1fd192ce482de
Michael <MichaelWidler@yahoo.com>
2026-08-21 12:51:23 -0700

    Make base tiles.

---
+++ Map/zone_constants.py
@@ -0,0 +1,24 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from GUI.gui_constants import *
+
+TileBases = [
+   dict(name="Pit", high_passable=True, low_passable=True, transparent=True, tile_index=' '),
+   dict(name="Clear", high_passable=True, low_passable=True, transparent=True, tile_index=TileConstants.BULLET_TILE),
+   dict(name="Low Wall", high_passable=True, low_passable=True, transparent=True, tile_index='='),
+   dict(name="Wall", high_passable=True, low_passable=True, transparent=True, tile_index='#'),
+   dict(name="Rough", high_passable=True, low_passable=True, transparent=True, tile_index=','),
+   dict(name="Bars", high_passable=True, low_passable=True, transparent=True, tile_index=':'),
+   dict(name="Shallow Liquid", high_passable=True, low_passable=True, transparent=True, tile_index='-'),
+   dict(name="Deep Liquid", high_passable=True, low_passable=True, transparent=True, tile_index='~'),
+   dict(name="Closed Door", high_passable=True, low_passable=True, transparent=True, tile_index='|'),
+   dict(name="Open Door", high_passable=True, low_passable=True, transparent=True, tile_index='/'),
+   dict(name="Terminal", high_passable=True, low_passable=True, transparent=True, tile_index=' '),
+   dict(name="Unflipped Switch", high_passable=True, low_passable=True, transparent=True, tile_index='!'),
+   dict(name="Flipped Switch", high_passable=True, low_passable=True, transparent=True, tile_index='`'),
+   dict(name="Breakable Container", high_passable=True, low_passable=True, transparent=True, tile_index='`'),
+   dict(name="Closed Chest", high_passable=True, low_passable=True, transparent=True, tile_index='`'),
+   dict(name="Opened Chest", high_passable=True, low_passable=True, transparent=True, tile_index='`')
+]
--- Map/zone_tile.py
+++ Map/zone_tile.py
@@ -6,7 +6,7 @@ from GUI import screen_obj
 
 class ZoneTile(ScreenObj):
    
-   def __init__(self, palette, index = ' ', fg = ColorConstants.WHITE, bg = None):
+   def __init__(self, palette, index = ' ', fg = ColorConstants.WHITE, bg = ColorConstants.BLACK):
       super().__init__(palette, index, fg, bg)
       self.low_passable = True
       self.high_passable = True
