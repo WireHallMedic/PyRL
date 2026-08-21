32184e93b5b1f87ff53c12f5a0e47705fb07f1f7
Michael <MichaelWidler@yahoo.com>
2026-08-21 12:28:49 -0700

    Create class for map tiles.

---
+++ Map/zone_tile.py
@@ -0,0 +1,13 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from GUI import screen_obj
+
+class ZoneTile(ScreenObj):
+   
+   def __init__(self, palette, index = ' ', fg = ColorConstants.WHITE, bg = None):
+      super().__init__(palette, index, fg, bg)
+      self.low_passable = True
+      self.high_passable = True
+      self.transparent = True
